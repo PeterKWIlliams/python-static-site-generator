72f66e265173782d0de6c1deabe169c7f255768b
Peter Williams <peterkennethwilliams@gmail.com>
2024-07-13 08:11:19 +0100

    Html Tags Enum
    Created enums for all required html tags that will be used throughout
    the project.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,27 @@
+from enum import Enum
+
+
+class HtmlNodeTag(Enum):
+    DIV = "div"
+    BOLD = "b"
+    ITALIC = "i"
+    CODE = "code"
+    HYPER_LINK = "a"
+    IMAGE = "img"
+    HEADING_1 = "h1"
+    HEADING_2 = "h2"
+    HEADING_3 = "h3"
+    HEADING_4 = "h4"
+    HEADING_5 = "h5"
+    HEADING_6 = "h6"
+    PARAGRAPH = "p"
+    LIST_ITEM = "li"
+    ORDERED_LIST = "ol"
+    UNORDERED_LIST = "ul"
+    QUOTE = "blockquote"
+    PRE_FORMATTED_TEXT = "pre"
+
+
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
